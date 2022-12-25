9427b20c18cacc200f7c0d81ac7e2dfbbc50ae6f
Dasic <dasicph@gmaio.com>
2022-12-25 11:47:21 +0700

    fix: Remove the invalid file property

--- PathsSetupTest/PathsResolverTest.cs
+++ PathsSetupTest/PathsResolverTest.cs
@@ -13,9 +13,10 @@ public class PathsResolverTest
             FolderPath = "folder/another folder/",
             FilePath = "test.json",
             FolderFilePath = "folder/another folder/test.json",
-            InvalidPath = "not_a_path",
             PathWithoutAttribute = "test.json"
         };
         new PathsResolver("").Resolve(testData);
+        new PathsResolver(Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), nameof(PathsResolverTest))).Resolve(testData);
     }
 }
--- PathsSetupTest/TestData.cs
+++ PathsSetupTest/TestData.cs
@@ -7,6 +7,5 @@ public struct TestData
     [Path] public string FolderPath { get; init; }
     [Path] public string FilePath { get; init; }
     [Path] public string FolderFilePath { get; init; }
-    [Path] public string InvalidPath { get; init; }
     public string PathWithoutAttribute { get; init; }
 }
